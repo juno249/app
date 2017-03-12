8c734d0bc9ccc0dac3579b1670e87c6d33b0ce76
johnvlim <johnvictormlim@gmail.com>
2017-03-12 16:29:05 -0700

    Changes Commit: (1) Updating source

--- menuGo_v1_ionic/ionic/www/templates/customer/nearby/menu/order/js/controllers/customerNearbyMenuOrderController.js
+++ menuGo_v1_ionic/ionic/www/templates/customer/nearby/menu/order/js/controllers/customerNearbyMenuOrderController.js
@@ -2,10 +2,38 @@ angular
 .module('starter')
 .controller('customerNearbyMenuOrderController', customerNearbyMenuOrderController);
 
+/* ******************************
+ * Controller Dependency Injection (Start)
+ * ****************************** */
 customerNearbyMenuOrderController.$inject = [
+	'$localStorage'
 ];
+/* ******************************
+ * Controller Dependency Injection (End)
+ * ****************************** */
 
-function customerNearbyMenuOrderController(		
+/* ******************************
+ * Controller Implementation (Start)
+ * ****************************** */
+function customerNearbyMenuOrderController(
+		$localStorage
 ){
-	alert('helloWorld');
+	const MY_ORDERS_KEY= 'My_Orders';
+	/* ******************************
+	 * Controller Binded Data (Start)
+	 * ****************************** */
+	var vm = this;
+	if(null == localStorage.getItem(MY_ORDERS_KEY)){
+		vm.myOrders = {};
+	} else {
+		var myOrders = localStorage.getItem(MY_ORDERS_KEY);
+		myOrders = JSON.parse(myOrders);
+		vm.myOrders = myOrders;
+	}
+	/* ******************************
+	 * Controller Binded Data (End)
+	 * ****************************** */
 }
+/* ******************************
+ * Controller Implementation (End)
+ * ****************************** */
