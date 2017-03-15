7e5d52bb9daf9822be5fc2aab192d66c6f379097
Lim, John Victor <johnvictor.lim@misys.com>
2017-03-15 20:07:07 +0800

    Changes Commit: (1) added reservationOrderreferenceOrderService

---
+++ menuGo_v1_ionic/ionic/www/templates/common/utilities/reservationOrderreferenceOrder/reservationOrderreferenceOrderService.js
@@ -0,0 +1,71 @@
+angular
+.module('starter')
+.factory('reservationOrderreferenceOrderService', reservationOrderreferenceOrderService);
+
+/* ******************************
+ * Service Dependency Injection (Start)
+ * ****************************** */
+reservationOrderreferenceOrderService.$inject = [
+	'$http', 
+	'$localStorage', 
+	'$q' 
+];
+/* ******************************
+ * Service Dependency Injection (End)
+ * ****************************** */
+
+/* ******************************
+ * Service Implementation (Start)
+ * ****************************** */
+function reservationOrderreferenceOrderService(
+		$http, 
+		$localStorage, 
+		$q
+){
+	/* ******************************
+	 * Service Return Object (Start)
+	 * ****************************** */
+	var reservationOrderreferenceOrderServiceObj = {
+			addReservationOrderreferenceOrder: addReservationOrderreferenceOrder
+	}
+	/* ******************************
+	 * Service Return Object (End)
+	 * ****************************** */
+	
+	/* ******************************
+	 * Method Implementation
+	 * method name: addReservationOrderreferenceOrder()
+	 * purpose: adds reservation, orderreference, order
+	 * ****************************** */
+	function addReservationOrderreferenceOrder(transParams){
+		var deferred = $q.defer();
+		var httpConfig = {
+				method: 'POST', 
+				url: API_BASE_URL + '/reservations-orderreferences-orders', 
+				data: transParams
+		}
+		$http(httpConfig)
+		.then(addReservationOrderreferenceOrderSuccessCallback)
+		.catch(addReservationOrderreferenceOrderFailedCallback);
+		
+		/* ******************************
+		 * Callback Implementations (Start)
+		 * ****************************** */
+		function addReservationOrderreferenceOrderSuccessCallback(response){
+			deferred.resolve(response);
+		}
+		
+		function addReservationOrderreferenceOrderFailedCallback(responseError){
+			deferred.reject(responseError);
+		}
+		/* ******************************
+		 * Callback Implementations (End)
+		 * ****************************** */
+		return deferred.promise;
+	}
+	
+	return reservationOrderreferenceOrderServiceObj;
+}
+/* ******************************
+ * Service Implementation (End)
+ * ****************************** */
