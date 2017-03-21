619c50417b57141b7e1ac5bb791426c6b4ecc7d8
johnvlim <johnvictormlim@gmail.com>
2017-03-21 16:16:46 -0700

    Changes Commit: (1) Minor code change

--- menuGo_v1_ionic/ionic/www/templates/customer/home/js/controllers/customerHomeController.js
+++ menuGo_v1_ionic/ionic/www/templates/customer/home/js/controllers/customerHomeController.js
@@ -26,8 +26,8 @@ function customerHomeController(
 		$scope, 
 		$timeout
 	){
-	$ionicHistory.clearHistory();
-	
+	const DOM_ADVERTISEMENTS_SLIDEBOX = 'advertisements-slidebox';
+	const DOM_BLOGS_SLIDEBOX = 'blogs-slidebox';
 	const MARKETING_KEY = 'Marketing';
 	/* ******************************
 	 * Controller Binded Data (Start)
@@ -36,27 +36,32 @@ function customerHomeController(
 	var marketing = undefined;
 	if(null == localStorage.getItem(MARKETING_KEY)){
 		dataService.fetchMarketing();
+		
 		vm.marketing = undefined;
+		vm.advertisements = undefined;
+		vm.blogs = undefined;
 	} else {
 		marketing = localStorage.getItem(MARKETING_KEY);
 		marketing = JSON.parse(marketing);
+		
 		vm.marketing = marketing;
+		vm.advertisements = vm.marketing.advertisements;
+		vm.blogs = vm.marketing.blogs;
 	}
-	vm.advertisements = undefined;
-	vm.blogs = undefined;
 	/* ******************************
 	 * Controller Binded Data (End)
 	 * ****************************** */
+	
+	$ionicHistory.clearHistory();
 	
 	/* ******************************
 	 * Watchers (Start)
 	 * ****************************** */
 	$scope.$watch(
-			function(){
-				return localStorage.getItem(MARKETING_KEY);
+			function(){	return localStorage.getItem(MARKETING_KEY);
 			}, 
 			function(){
-				var marketing = vm.marketing;
+				var marketing = undefined;
 				
 				marketing = localStorage.getItem(MARKETING_KEY);
 				marketing = JSON.parse(marketing);
@@ -66,30 +71,26 @@ function customerHomeController(
 	);
 	
 	$scope.$watch(
-			function(){
-				return vm.marketing;
+			function(){	return vm.marketing;
 			}, 
 			function(){
-				var marketing = vm.marketing;
-				var advertisements = vm.advertisements;
-				var blogs = vm.blogs;
-				var advertisementsSlidebox = 'advertisements-slidebox';
-				var blogsSlidebox = 'blogs-slidebox';
+				var marketing = undefined;
+				var advertisements = undefined;
+				var blogs = undefined;
 				
-				if(null == marketing){
-					return;
+				marketing = vm.marketing;
+				if(!(null == marketing)){
+					advertisements = marketing.advertisements;
+					blogs = marketing.blogs;
+					
+					vm.advertisements = advertisements;
+					vm.blogs = blogs;
 				}
-				
-				advertisements = marketing.advertisements;
-				blogs = marketing.blogs;
-				
-				vm.advertisements = advertisements;
-				vm.blogs = blogs;
-				
+
 				$timeout(function(){
-					$ionicSlideBoxDelegate.$getByHandle(advertisementsSlidebox).update();
-					$ionicSlideBoxDelegate.$getByHandle(blogsSlidebox).update();
-				});
+					$ionicSlideBoxDelegate.$getByHandle(DOM_ADVERTISEMENTS_SLIDEBOX).update();
+					$ionicSlideBoxDelegate.$getByHandle(DOM_BLOGS_SLIDEBOX).update();
+				}, 1000);
 			}
 	);
 	/* ******************************
