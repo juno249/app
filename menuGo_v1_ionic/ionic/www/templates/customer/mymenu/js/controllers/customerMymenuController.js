03aa8facd4e11e62f292b6af8b0935a687ce87dc
johnvlim <johnvictormlim@gmail.com>
2017-04-25 22:56:53 -0700

    Changes Commit: (1) Updating code source

--- menuGo_v1_ionic/ionic/www/templates/customer/mymenu/js/controllers/customerMymenuController.js
+++ menuGo_v1_ionic/ionic/www/templates/customer/mymenu/js/controllers/customerMymenuController.js
@@ -10,6 +10,11 @@ customerMymenuController.$inject = [
 
 function customerMymenuController(
 		){
+	const DOM_ION_HEADER_BAR_TAG = 'ion-header-bar';
+	const DOM_ION_TABS_CLASS = '.tab-nav.tabs';
+	const DOM_RESERVATION_DETAIL_CONTAINER = '#reservation-detail-container';
+	const DOM_BUTTON_CONTAINER = '#button-container';
+	
 	var vm = this;
 	
 	//dummy data - test (start)
@@ -17,4 +22,23 @@ function customerMymenuController(
 	vm.branchName = 'Ermita';
 	vm.tableNumber = 1;
 	//dummy data - test (end)
+	
+	//controller_method
+	vm.initDom = initDom;
+	
+	function initDom(){
+		var domIonNavBar = $(DOM_ION_HEADER_BAR_TAG).eq(0);
+		var domIonTabs = $(DOM_ION_TABS_CLASS).eq(0);
+		var domReservationDetailContainer = $(DOM_RESERVATION_DETAIL_CONTAINER).eq(0);
+		var domButtonContainer = $(DOM_BUTTON_CONTAINER).eq(0);
+		
+		domReservationDetailContainer.css(
+				'top', 
+				domIonNavBar[0].clientHeight
+				);
+		domButtonContainer.css(
+				'bottom', 
+				domIonTabs[0].clientHeight
+				);
+		}
 	}
