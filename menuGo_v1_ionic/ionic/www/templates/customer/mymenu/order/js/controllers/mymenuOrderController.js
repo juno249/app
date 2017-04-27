dba0dafaecd133cbfd93e1a6ac2d17347eb5202a
johnvlim <johnvictormlim@gmail.com>
2017-04-26 23:47:34 -0700

    Changes Commit: (1) Updating code source

--- menuGo_v1_ionic/ionic/www/templates/customer/mymenu/order/js/controllers/mymenuOrderController.js
+++ menuGo_v1_ionic/ionic/www/templates/customer/mymenu/order/js/controllers/mymenuOrderController.js
@@ -6,10 +6,43 @@ angular
 		);
 
 mymenuOrderController.$inject = [
-	'$scope'
+	'$scope', 
+	'dataService', 
+	'reservationOrderreferenceOrderService'
 	];
 
 function mymenuOrderController(
-		$scope
+		$scope, 
+		dataService, 
+		reservationOrderreferenceOrderService
 		){
-}
+	const COMPANIES_KEY = 'Companies';
+	const USER_KEY = 'User';
+	
+	var vm = this;
+	vm.companyName = $scope.$parent.customerMymenuController.companyName;
+	vm.branchName = $scope.$parent.customerMymenuController.branchName;
+	vm.tableNumber = $scope.$parent.customerMymenuController.tableNumber;
+	
+	if(!(null == localStorage.getItem(COMPANIES_KEY))){
+		vm.companies = localStorage.getItem(COMPANIES_KEY);
+		vm.companies = JSON.parse(vm.companies);
+	} else {	dataService.fetchCompanies();
+	}
+	
+	if(!(null == localStorage.getItem(USER_KEY))){
+		vm.user = localStorage.getItem(USER_KEY);
+		vm.user = JSON.parse(vm.user);
+		}
+	
+	reservationOrderreferenceOrderService.setCustomerUsername(vm.user.username);
+	reservationOrderreferenceOrderService.fetchReservationsOrderreferencesOrders()
+	.then(fetchReservationsOrderreferencesOrdersSuccessCallback)
+	.catch(fetchReservationsOrderreferencesOrdersFailedCallback);
+	
+	function fetchReservationsOrderreferencesOrdersSuccessCallback(response){	//do something on success
+	}
+	
+	function fetchReservationsOrderreferencesOrdersFailedCallback(responseError){	//do something on failure
+	}
+	}
