9cb556e310e540b687a7068f515194f3ad5e313b
johnvlim <johnvictormlim@gmail.com>
2017-05-01 23:07:37 -0700

    Changes Commit: (1) Updating code source

--- menuGo_v1_ionic/ionic/www/templates/common/utilities/network/networkService.js
+++ menuGo_v1_ionic/ionic/www/templates/common/utilities/network/networkService.js
@@ -6,10 +6,30 @@ angular
 		);
 
 networkService.$inject = [
+	'$cordovaNetwork', 
 	'$rootScope'
 	];
 
 function networkService(
+		$cordovaNetwork, 
 		$rootScope
 		){
+	networkServiceObj = {
+			deviceIsOnline: deviceIsOnline, 
+			deviceIsOffline: deviceIsOffline, 
+			};
+	
+	function deviceIsOnline(){
+		if(ionic.Platform.isWebView()){	return $cordovaNetwork.isOnline();
+		} else {	return navigator.onLine;
+		}
+		}
+	
+	function deviceIsOffline(){
+		if(ionic.Platform.isWebView()){	return !$cordovaNetwork.isOnline();
+		} else {	return !navigator.onLine;
+		}
+		}
+	
+	return networkServiceObj;
 	}
