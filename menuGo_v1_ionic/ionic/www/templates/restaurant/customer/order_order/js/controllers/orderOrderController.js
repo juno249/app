f931eb8c3d24fa2ed565cdca0d3b5b5617477045
johnvlim <johnvictormlim@gmail.com>
2017-05-06 20:50:34 -0700

    Changes Commit: (1) Updating code source

---
+++ menuGo_v1_ionic/ionic/www/templates/restaurant/customer/order_order/js/controllers/orderOrderController.js
@@ -0,0 +1,93 @@
+angular
+.module('starter')
+.controller(
+		'orderOrderController', 
+		orderOrderController
+		);
+
+orderOrderController.$inject = [
+	'ERROR_MESSAGES', 
+	'KEYS', 
+	'LOADING_MESSAGES', 
+	'ORDER_STATUS', 
+	'ORDERREFERENCE_STATUS', 
+	'PAYMENT_MODES', 
+	'$localStorage', 
+	'$scope', 
+	'orderService', 
+	'orderreferenceService', 
+	'orderreferenceOrderService', 
+	'popupService'
+	];
+
+function orderOrderController(
+		ERROR_MESSAGES, 
+		KEYS, 
+		LOADING_MESSAGES, 
+		ORDER_STATUS, 
+		ORDERREFERENCE_STATUS, 
+		$localStorage, 
+		$scope, 
+		orderService, 
+		orderreferenceService, 
+		orderreferenceOrderService, 
+		popupService
+		){
+	var vm = this;
+	
+	//controller_method
+	vm.remReservationOrder = remReservationOrder;
+	//controller_method
+	vm.getTotalCost = getTotalCost;
+	//controller_method
+	vm.PostOrderreference = PostOrderreference;
+	
+	function remReservationOrder(menuitem){
+		delete vm.user.reservationOrder[menuitem.menuitem_code];
+		
+		localStorage.setItem(
+				KEYS.User, 
+				JSON.stringify(vm.user)
+				);
+		}
+	
+	function getTotalCost(){
+		var totalCost = 0;
+		
+		if(null == vm.user.reservationOrder){	return;
+		}
+		
+		angular.forEach(
+				vm.user.reservationOrder, 
+				function(
+						v, 
+						k
+						){	totalCost += v.quantity * v.menuitem_price;
+						}
+				);
+		
+		return totalCost;
+		}
+	
+	function PostOrderreference(){
+	}
+	
+	$scope.$watch(
+			function(){	return localStorage.getItem(KEYS.User);
+			}, 
+			function(){
+				vm.user = localStorage.getItem(KEYS.User);
+				vm.user = JSON.parse(vm.user);
+				
+				if(null == vm.user.reservationOrder){	vm.user.reservationOrder = {};
+				}
+				}
+			);
+	
+	$scope.$watchCollection(
+			function(){	return vm.user.reservationOrder;
+			}, 
+			function(){	getTotalCost();
+			}
+			);
+	}
