76502c2476075c92990374e60c334e7c53fce33a
johnvlim <johnvictormlim@gmail.com>
2017-05-06 19:38:11 -0700

    Changes Commit: (1) Updating code source

---
+++ menuGo_v1_ionic/ionic/www/templates/restaurant/customer/order_menu/js/controllers/orderMenuController.js
@@ -0,0 +1,218 @@
+angular
+.module('starter')
+.controller(
+		'orderMenuController', 
+		orderMenuController
+		);
+
+orderMenuController.$inject = [
+	'BROADCAST_MESSAGES', 
+	'ERROR_MESSAGES', 
+	'KEYS', 
+	'LOADING_MESSAGES', 
+	'$localStorage', 
+	'$scope', 
+	'dataService', 
+	'networkService', 
+	'popupService'
+	];
+
+function orderMenuController(
+		BROADCAST_MESSAGES, 
+		ERROR_MESSAGES, 
+		KEYS, 
+		LOADING_MESSAGES, 
+		$localStorage, 
+		$scope, 
+		dataService, 
+		networkService, 
+		popupService
+		){
+	var vm = this;
+	
+	if(
+			networkService.deviceIsOffline() &&
+			!(null == localStorage.getItem(KEYS.Companies))
+			){
+		vm.company = localStorage.getItem(KEYS.Companies);
+		vm.company = JSON.parse(vm.company);
+		} else if(
+				networkService.deviceIsOffline() &&
+				null == localStorage.getItem(KEYS.Companies)
+				){
+			vm.company = {};
+			vm._company = {};
+			vm.companyMenu = {};
+			vm.companyMenuMenuitem = {};
+			} else {
+				dataService.fetchCompanies();
+				
+				popupService.dispIonicLoading(LOADING_MESSAGES.gettingData);
+				}
+	
+	//controller_method
+	vm.gotoState = gotoState;
+	//controller_method
+	vm.setMenuName = setMenuName;
+	//controller_method
+	vm.getMenuIdFromName = getMenuIdFromName;
+	//controller_method
+	vm.addReservationOrder = addReservationOrder;
+	//controller_method
+	vm.subReservationOrder = subReservationOrder;
+	
+	function gotoState(stateName){
+	}
+	
+	function setMenuName(menuName){	vm.menuName = menuName;
+	}
+	
+	function getMenuIdFromName(){	return vm.companyMenu[vm.menuName].menu_id;
+	}
+	
+	function addReservationOrder(menuitem){
+		menuitem.quantity++;
+		
+		vm.user.reservationOrder[menuitem.menuitem_code] = menuitem;
+		localStorage.setItem(
+				KEYS.User, 
+				JSON.stringify(vm.user)
+				);
+		}
+	
+	function subReservationOrder(menuitem){
+		if(0 >= --menuitem.quantity){
+			menuitem.quantity = 0;
+			
+			delete vm.user.reservationOrder[menuitem.menuitem_code];
+			} else {	vm.user.reservationOrder[menuitem.menuitem_code] = menuitem;
+			}
+		
+		localStorage.setItem(
+				KEYS.User, 
+				JSON.stringify(vm.user)
+				);
+		}
+	
+	function genCompanyMenuMenuitem(){
+		var companyMenuMenuitem = {};
+		
+		angular.forEach(
+				vm.companyMenu, 
+				function(
+						v, 
+						k
+						){
+					if(null == vm.menuName){	vm.menuName = k;
+					}
+					angular.forEach(
+							v.menuitems, 
+							function(
+									v, 
+									k
+									){	companyMenuMenuitem[v.menuitem_code] = v;
+									}
+							);
+					}
+				);
+		
+		return companyMenuMenuitem;
+		}
+	
+	function resetCompanyMenuMenuitem(){
+		angular.forEach(
+				vm.companyMenuMenuitem, 
+				function(
+						v, 
+						k
+						){	v.quantity = 0;
+						}
+				);
+		}
+	
+	function synchronize(){
+		angular.forEach(
+				vm.companyMenuMenuitem, 
+				function(
+						v, 
+						k
+						){
+					if(!(null == vm.user.reservationOrder[v.menuitem_code])){	v.quantity = vm.user.reservationOrder[v.menuitem_code].quantity;
+					} else {	v.quantity = 0;
+					}
+					}
+				);
+		}
+	
+	$scope.$watch(
+			function(){	return localStorage.getItem(KEYS.Companies);
+			}, 
+			function(){
+				vm.company = localStorage.getItem(KEYS.Companies);
+				vm.company = JSON.parse(vm.company);
+				}
+			);
+	
+	$scope.$watchCollection(
+			function(){	return vm.company;
+			}, 
+			function(){
+				if(!(null == vm.company)){
+					vm._company = vm.company[vm.companyName];
+					if(null == vm._company){	return;
+					}
+					
+					if(!(null == vm._company.branches)){
+						vm._branch = vm._company.branches[vm.branchName];
+						if(null == vm._branch){	return;
+						}
+						
+						if(!(null == vm._branch.tables)){	vm._table = vm._branch.tables[vm.tableNumber];
+						}
+						}
+					
+					if(!(null == vm._company.menus)){	vm.companyMenu = vm._company.menus;
+					}
+					}
+				
+				vm.companyMenuMenuitem = genCompanyMenuMenuitem();
+				resetCompanyMenuMenuitem();
+				}
+			);
+	
+	$scope.$watch(
+			function(){	return localStorage.getItem(KEYS.User);
+			}, 
+			function(){
+				vm.user = localStorage.getItem(KEYS.User);
+				vm.user = JSON.parse(vm.user);
+				
+				if(null == vm.user.reservationOrder){	vm.user.reservationOrder = {};
+				}
+				}
+			);
+	
+	$scope.$watchCollection(
+			function(){	return vm.user.reservationOrder;
+			}, 
+			function(){	synchronize();
+			}
+			);
+	
+	$scope.$on(
+			BROADCAST_MESSAGES.getCompaniesSuccess, 
+			function(){	popupService.hideIonicLoading();
+			}
+			);
+	
+	$scope.$on(
+			BROADCAST_MESSAGES.getCompaniesFailed, 
+			function(){
+				var DOM_POPUP_CLASS = '.popup';
+				
+				popupService.hideIonicLoading();
+				if(0 == $(DOM_POPUP_CLASS).length){	popupService.dispIonicPopup(ERROR_MESSAGES.getFailed);
+				}
+				}
+			);
+	}
