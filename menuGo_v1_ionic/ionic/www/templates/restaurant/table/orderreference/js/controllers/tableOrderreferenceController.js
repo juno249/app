68cdf85e93126d66e8f6d7e0f85d5a67c5fba2f5
johnvlim <johnvictormlim@gmail.com>
2017-05-13 22:07:14 -0700

    Changes Cmmit: (1) Updating code source

--- menuGo_v1_ionic/ionic/www/templates/restaurant/table/orderreference/js/controllers/tableOrderreferenceController.js
+++ menuGo_v1_ionic/ionic/www/templates/restaurant/table/orderreference/js/controllers/tableOrderreferenceController.js
@@ -6,16 +6,127 @@ angular
 		);
 
 tableOrderreferenceController.$inject = [
-                                         '$stateParams'
+                                         'ERROR_MESSAGES', 
+                                         'KEYS', 
+                                         'LOADING_MESSAGES', 
+                                         '$scope', 
+                                         '$stateParams', 
+                                         'networkService', 
+                                         'orderreferenceOrderService', 
+                                         'popupService', 
+                                         'reservationOrderreferenceOrderService'
                                          ];
 
 function tableOrderreferenceController(
-		$stateParams
+		ERROR_MESSAGES, 
+		KEYS, 
+		LOADING_MESSAGES, 
+		$scope, 
+		$stateParams, 
+		networkService, 
+		orderreferenceOrderService, 
+		popupService, 
+		reservationOrderreferenceOrderService
 		){
 	var vm = this;
+	vm.isReservation = false;
 	
 	if(!(null == $stateParams.companyName)){	vm.companyName = $stateParams.companyName;
 	}
 	if(!(null == $stateParams.branchName)){	vm.branchName = $stateParams.branchName;
 	}
+	
+	if(networkService.deviceIsOffline()){
+		vm.companyBranchReservation = {};
+		vm.companyBranchOrderreference = {};
+	} else {
+		popupService.dispIonicLoading(LOADING_MESSAGES.gettingData);
+		
+		reservationOrderreferenceOrderService.setCompanyName(vm.companyName);
+		reservationOrderreferenceOrderService.setBranchName(vm.branchName);
+		reservationOrderreferenceOrderService.fetchReservationsOrderreferencesOrders(4)
+		.then(fetchReservationsOrderreferencesOrdersSuccessCallback)
+		.catch(fetchReservationsOrderreferencesOrdersFailedCallback);
+		}
+	
+	function fetchReservationsOrderreferencesOrdersSuccessCallback(response){
+		vm.companyBranchReservation = {};
+		
+		angular.forEach(
+				response, 
+				function(
+						v, 
+						k
+						){
+					vm.companyBranchReservation[k] = {};
+					vm.companyBranchReservation[k].reservation = {};
+					vm.companyBranchReservation[k].reservation[v.reservations.reservation_code] = v.reservations;
+					vm.companyBranchReservation[k].orderreference = {};
+					vm.companyBranchReservation[k].orderreference[v.orderreferences.orderreference_code] = v.orderreferences;
+					vm.companyBranchReservation[k].order = {};
+					vm.companyBranchReservation[k].order= v.orders;
+					vm.companyBranchReservation[k].table = {};
+					vm.companyBranchReservation[k].table[v.tables.table_number] = v.tables;
+					}
+				);
+		
+		orderreferenceOrderService.setCompanyName(vm.companyName);
+		orderreferenceOrderService.setBranchName(vm.branchName);
+		orderreferenceOrderService.fetchOrderreferencesOrders(4)
+		.then(fetchOrderreferencesOrdersSuccessCallback)
+		.catch(fetchOrderreferencesOrdersFailedCallback);
+		
+		function fetchOrderreferencesOrdersSuccessCallback(response){
+			vm.companyBranchOrderreference = {};
+			
+			angular.forEach(
+					response, 
+					function(
+							v, 
+							k
+							){
+						vm.companyBranchOrderreference[k] = {};
+						vm.companyBranchOrderreference[k].orderreference = {};
+						vm.companyBranchOrderreference[k].orderreference[v.orderreferences.orderreference_code] = v.orderreferences;
+						vm.companyBranchOrderreference[k].order = {};
+						vm.companyBranchOrderreference[k].order = v.orders;
+						vm.companyBranchOrderreference[k].table = {};
+						vm.companyBranchOrderreference[k].table[v.tables.table_number] = v.tables;
+						}
+					);
+			
+			angular.forEach(
+					vm.companyBranchOrderreference, 
+					function(
+							v, 
+							k
+							){
+						angular.forEach(
+								vm.companyBranchReservation, 
+								function(
+										j, 
+										i
+										){
+									if(!(null == j.orderreference[k])){	delete vm.companyBranchOrderreference[k];
+									}
+									}
+								);
+						}
+					);
+			
+			popupService.hideIonicLoading();
+			}
+		
+		function fetchOrderreferencesOrdersFailedCallback(responseError){
+			popupService.hideIonicLoading();
+			
+			popupService.dispIonicPopup(ERROR_MESSAGES.getFailed);
+			}
+		}
+	
+	function fetchReservationsOrderreferencesOrdersFailedCallback(responseError){
+		popupService.hideIonicLoading();
+		
+		popupService.dispIonicPopup(ERROR_MESSAGES.getFailed);
+		}
 	}
